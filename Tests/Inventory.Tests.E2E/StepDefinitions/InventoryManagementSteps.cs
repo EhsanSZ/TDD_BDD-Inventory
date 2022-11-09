d60a9ea92cb20e9dbb7803efa715e38755873eea
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-11-09 21:01:48 +0330

    Test E2E

--- Tests/Inventory.Tests.E2E/StepDefinitions/InventoryManagementSteps.cs
+++ Tests/Inventory.Tests.E2E/StepDefinitions/InventoryManagementSteps.cs
@@ -1,31 +1,41 @@
 ﻿using System.Net;
 using FluentAssertions;
 using Newtonsoft.Json;
+using FluentAssertions;
+using Inventory.Tests.E2E.Tools;
+using RestSharp;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
+using Inventory.Application.Contract;
 
 namespace Inventory.Tests.E2E.Steps
 {
     [Binding]
     public class InventoryManagementSteps
     {
-
+        private bool _postResult;
+        private DefineInventory _command;
 
         [Given(@"I Want To Add The Following Inventory")]
         public void GivenIWantToAddTheFollowingInventory(Table table)
         {
-
+            _command = table.CreateInstance<DefineInventory>();
         }
 
         [When(@"I Try To Define This Inventory")]
         public void WhenITryToDefineThisInventory()
         {
+            var restClient = new RestClient(HostConstants.Endpoint);
+            var restRequest = new RestRequest("Inventory");
+            restRequest.AddJsonBody(_command);
+            var response = restClient.Post(restRequest);
+            _postResult = JsonConvert.DeserializeObject<bool>(response.Content);
         }
 
         [Then(@"The Inventory Should Be Created")]
         public void ThenTheInventoryShouldBeCreated()
         {
-
+            _postResult.Should().BeTrue();
         }
 
         [Then(@"It Should Be Empty")]
---
+++ Tests/Inventory.Tests.E2E/Tools/HostConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory.Tests.E2E.Tools
+{
+    public static class HostConstants
+    {
+        public const int Port = 5001;
+        public static readonly string Endpoint = $"https://localhost:{Port}/api";
+
+        public const string CsProjectPath =
+            @"C:\Hossein\Packages\Test (TDD&BDD)\examples\14-FinalProject\InventoryManagement\src\Inventory.Presentation\Inventory.Presentation.csproj";
+    }
+}
