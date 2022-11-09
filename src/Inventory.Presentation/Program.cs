f54878dd7c3baee337b480ceff520bae0994d1e1
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-11-09 14:23:34 +0330

    Infrastructure

--- src/Inventory.Presentation/Program.cs
+++ src/Inventory.Presentation/Program.cs
@@ -1,26 +1,42 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Inventory.Application;
+using Inventory.Application.Contract;
+using Inventory.Domain;
+using Inventory.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
-namespace Inventory.Presentation
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IInventoryService, InventoryService>();
+builder.Services.AddTransient<IInventoryRepository, InventoryRepository>();
+
+var config = builder.Configuration;
+
+var connectionString = config.GetConnectionString("InventoryDb");
+builder.Services
+    .AddDbContext<InventoryContext>(builder =>
+        builder.UseSqlServer(connectionString));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
